c60f2d57693569a4e300f0fcf21e3a2bac567a0b
attalariq123 <attalariq002@gmail.com>
2023-03-04 10:19:11 +0700

    initial commit

---
+++ app/repository/teacher.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"a21hc3NpZ25tZW50/app/model"
+
+	"gorm.io/gorm"
+)
+
+type TeacherRepo struct {
+	db *gorm.DB
+}
+
+func NewTeacherRepo(db *gorm.DB) TeacherRepo {
+	return TeacherRepo{db}
+}
+
+func (u *TeacherRepo) AddTeacher(teacher model.Teacher) error {
+	return u.db.Model(model.Teacher{}).Create(&teacher).Error
+}
+
+func (u *TeacherRepo) ReadTeacher() ([]model.ViewTeacher, error) {
+
+	var teacherList []model.ViewTeacher
+	err := u.db.Table("teachers").Select("name, field_of_study, age").Where("deleted_at is NULL").Find(&teacherList).Error
+
+	return teacherList, err
+}
+
+func (u *TeacherRepo) UpdateName(id uint, name string) error {
+
+	err := u.db.Model(model.Teacher{}).Where("id = ?", id).Update("name", name).Error
+
+	return err
+}
+
+func (u *TeacherRepo) DeleteTeacher(id uint) error {
+
+	err := u.db.Where("id = ?", id).Delete(&model.Teacher{}).Error
+
+	return err
+}
